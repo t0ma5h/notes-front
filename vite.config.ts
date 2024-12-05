36ba775271ac3945b1613236469ce5e77ea8dbb2
Tomas S <tomas.sukelovic@gmail.com>
2024-12-05 10:18:00 +0200

    fix: gh pgaes

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import tailwindcss from "tailwindcss";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/notes-front",
+  base: "/notes-front/",
   css: {
     postcss: {
       plugins: [tailwindcss()],
